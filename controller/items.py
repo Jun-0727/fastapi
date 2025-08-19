f3f6ca3a55baf565b8b692c5c4b3ed6a4a1e9602
jetty <dlwnsdud980727@gmail.com>
2025-08-19 18:58:16 +0900

    POST 요청 처리 with.Request

--- controller/items.py
+++ controller/items.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 router = APIRouter(
     prefix="/items",
@@ -13,3 +13,14 @@ def read_item(item_id: int, category: str = None):
         "제품번호": item_id,
         "제품분류": category
         }
+
+# 형식이 없는 request body 데이터 파싱    
+@router.post('/')
+async def create_item(request: Request):
+    """
+    1. request body에 어떤 데이터가 들어있어도 됨
+    2. async/await로 비동기 처리 필수
+    """
+    data = await request.json()
+    
+    return {"data": data}
